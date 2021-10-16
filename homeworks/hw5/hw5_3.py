237ce07cd5441c2113e84e80b4251905fec30776
tamara <trych.td@mail.ru>
2021-10-16 19:15:35 +0300

    hw5_3

--- homeworks/hw5/hw5_3.py
+++ homeworks/hw5/hw5_3.py
@@ -3,3 +3,11 @@
 которая принимает три позиционных аргумента,
 и возвращает сумму наибольших двух аргументов.
 """
+
+def my_func(a, b, c):
+    temp_list = [a, b, c]
+    temp_list.sort(reverse=1)
+    _ = temp_list.pop()
+    print(sum(temp_list))
+
+my_func(-2, 554, 0)
