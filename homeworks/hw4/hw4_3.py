90ef20fea764bcd680f0a503873736222d2b6ca9
tamara <trych.td@mail.ru>
2021-10-17 19:56:28 +0300

    hw4_3

--- homeworks/hw4/hw4_3.py
+++ homeworks/hw4/hw4_3.py
@@ -3,3 +3,18 @@
 Определить Бьет ли Ферзь пешку
 координаты хранятся (x, y)
 """
+messages = (
+    'Ферзь бьет пешку',
+    'Ферзь не бьет пешку'
+)
+queen = (1, 6)
+pawn = (2, 6)
+
+difference = []
+if queen[0] == pawn[0] or queen[1] == pawn[1]:
+    print(messages[0])
+else:
+    for x, y in zip(queen, pawn):
+        difference.append(abs(x - y))
+    print(messages[0]) if difference[0] == difference[1] else print(messages[1])
+
